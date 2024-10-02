35b29b347af6ece58232303deaeca922e75e5804
Chloe Riviere <chloerivs@gmail.com>
2024-10-02 00:29:43 +0000

    Create post list view

--- data/views.py
+++ data/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+# Community posts: list view
+class PostListView(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = 'templates/data/post_list.html'
+    context_object_name = 'posts'
